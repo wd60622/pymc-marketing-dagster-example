0a5906b61848468605db7cdb56b5bfcc30e94488
Will Dean <wd60622@gmail.com>
2024-12-08 20:40:41 +0100

    create instances of these managers

--- pymc_marketing_dagster_example/definitions.py
+++ pymc_marketing_dagster_example/definitions.py
@@ -1,13 +1,29 @@
 from dagster import (
     Definitions,
+    io_manager,
     load_assets_from_modules,
 )
 
-from pymc_marketing_dagster_example import assets  # noqa: TID252
+from pymc_marketing_dagster_example import assets
+from pymc_marketing_dagster_example.io_manager import MMMIOManager, XArrayIOManager
 
 all_assets = load_assets_from_modules([assets])
 
 
+@io_manager
+def mmm_io_manager():
+    return MMMIOManager()
+
+
+@io_manager
+def xarray_io_manager():
+    return XArrayIOManager()
+
+
 defs = Definitions(
     assets=all_assets,
+    resources={
+        "mmm_io_manager": mmm_io_manager,
+        "netcdf_io_manager": xarray_io_manager,
+    },
 )
